028f5585165dcf68875d89f4e2c8f1e8ce600f7a
G4jw0r <szymon.domalik@microsoft.wsei.edu.pl>
2019-02-16 19:57:11 +0100

    Added function for pushing new note into notes array. Added function for createing new dives with content of note. Keep notes in local storage.

--- Projekt5/main.js
+++ Projekt5/main.js
@@ -11,7 +11,7 @@ function appStart(){
         addNotesToNotesContainer(note)
     }); 
 }
-
+//if there is title or content note is added to notes array with parameters
 function addNewNote() {
     const title   = document.querySelector("#NewNoteName").value
     const content = document.querySelector("#NewNoteContent").value
@@ -26,23 +26,30 @@ function addNewNote() {
     
 }
 
+//creates dives every time note is added to array and fill it with content
 function addNotesToNotesContainer(note){
-    const noteDateTime = new Date(note.id)
+    const noteDateTime = new Date(note.id).toUTCString()
 
     let noteDiv = document.createElement('div')
-    noteDiv.classList.add('note')
-    noteDiv.innerHTML ='\
-    <div class="note-title">$(title)</div>\
-    <div class="note-content">$(content)</div>\
-    <div class="note-date">$(content.id)</div>\
-    '
+    noteDiv.classList.add('note');
+    document.getElementById('NotesContainer').appendChild(noteDiv);
+    noteDiv.innerHTML =`\
+    <div class="note-title">${note.title}</div>\
+    <div class="note-content">${note.content}</div>\
+    <div class="note-date">${noteDateTime}</div>\
+    
+    `
+
 }
+
+
 function updateLocalStorage(){
     localStorage.setItem('notes', JSON.stringify(notes))
 }
 
+//creates new note with date when its added
 function Note(title = "", content = ""){
     this.title = title;
     this.content = content;
-    this.id = Date.now()
+    this.id = Date.now();
 }
